4f219dab1cb9da47a136b313c8a745588a1e8ef5
Stefano Formicola <stefano.formicola@mia-care.io>
2022-04-19 21:35:21 +0200

    envs renamed mock envs

--- src/typings/react-native-config.d.ts
+++ src/typings/react-native-config.d.ts
@@ -1,6 +1,9 @@
 declare module 'react-native-config' {
   const Config: {
     ENV: 'development' | 'preproduction' | 'production'
+    API_KEY_PARAM_NAME: string
+    API_KEY_PARAM_VALUE: string
+    BASE_URL: string
   }
   export default Config
 }
