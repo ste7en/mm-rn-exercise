fb635db6e3cc40e9f05cf7c5d01ed9fef01db644
Stefano Formicola <stefano.formicola@mia-care.io>
2022-04-20 12:11:56 +0200

    jest setup

--- jest.config.js
+++ jest.config.js
@@ -6,10 +6,17 @@ module.exports = {
       'identity-obj-proxy'
   },
   testEnvironment: 'jsdom',
+  setupFiles: [
+    './config/jest.setup.js',
+    './config/jest.redux.setup.js'
+  ],
   collectCoverageFrom: [
     'src/**/*.{ts,tsx}',
     '!src/**/*.test.{ts,tsx}',
     '!src/App.tsx',
     '!**/node_modules/**'
+  ],
+  transformIgnorePatterns: [
+    'node_modules/(?!(jest-)?react-native|@react-native|react-(native|universal|navigation)-(.*)|@react-native-community/(.*)|@react-navigation/(.*))'
   ]
 }
